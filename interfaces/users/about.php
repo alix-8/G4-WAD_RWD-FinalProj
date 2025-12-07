76c4135c5f29f4e2bba629773122808f86297cc9
alix <alexxxx.anmtr08@gmail.com>
2025-12-07 19:53:55 +0800

    added nav sa about

--- interfaces/users/about.php
+++ interfaces/users/about.php
@@ -10,7 +10,28 @@
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<body>
-    
+<body class="m-0 border-0 bd-example">
+<nav class="navbar p-3 sticky-top">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <img src="/assets/hamburger.png" alt="hamburger icon" width="20px" height="20px">
+        </button>
+        <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1"
+             id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+            <div class="offcanvas-body">
+                <a href="dashboard_admin.php" id="active_button">Dashboard</a>
+                <a href="myposts_admin.php">My Posts</a>
+                <a href="#">About</a>
+                <a class="logout" href="#">Log out</a>
+            </div>
+        </div>
+        <strong><a class="navbar-brand me-auto" href="#">Campus<span class = "find">Find</a></strong>
+        <a class="navbar-brand ms-auto text-white" href="#">Hello, <?php echo htmlspecialchars($admin["username"]); ?></a>
+    </div>
+</nav>
+
+
 </body>
 </html>
