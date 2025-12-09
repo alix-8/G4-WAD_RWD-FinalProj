96ff9007a677ae0a03751fa8f9d41bf669b28204
stefoine <anne.stephanne15@gmail.com>
2025-12-09 15:18:27 +0800

    Update about.php

--- interfaces/users/about.php
+++ interfaces/users/about.php
@@ -29,7 +29,7 @@
             </div>
         </div>
         <strong><a class="navbar-brand me-auto" href="#">Campus<span class = "find">Find</a></strong>
-        <a class="navbar-brand ms-auto text-white" href="#">Hello, <?php echo htmlspecialchars($user["username"]); ?></a>
+        <a class="navbar-brand ms-auto text-white" href="#">Hello, <?php echo htmlspecialchars($users["username"]); ?></a>
     </div>
 </nav>
 
